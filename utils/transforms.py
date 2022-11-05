b2844e58faccdfd41dccfd0b82514b0e6ee0cca6
jobin <jobin.kv@research.iiit.ac.in>
2022-11-05 19:44:36 +0530

    channel_axis

--- utils/transforms.py
+++ utils/transforms.py
@@ -98,6 +98,6 @@ class PortionCrop(object):
 class RandomGaussianBlur(object):
     def __call__(self, img):
         sigma = 0.15 + random.random() * 1.15
-        blurred_img = gaussian(np.array(img), sigma=sigma, multichannel=True)
+        blurred_img = gaussian(np.array(img), sigma=sigma, channel_axis=True)
         blurred_img *= 255
         return Image.fromarray(blurred_img.astype(np.uint8))
